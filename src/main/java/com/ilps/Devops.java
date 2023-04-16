e82c50d0051f98dea884972155f780244cf2b167
Mahesh <mahimd88@gmail.com>
2023-04-16 20:39:03 +0530

    mvn commiit jdk17

--- src/main/java/com/ilps/Devops.java
+++ src/main/java/com/ilps/Devops.java
@@ -4,7 +4,7 @@ public class Devops {
 
 	public static void main(String[] args) {
 		System.out.println("Devops here");
-		System.out.println("Devops herge");
+		System.out.println("Devops herge at jdk17");
 		// TODO Auto-generated method stub
 
 	}
